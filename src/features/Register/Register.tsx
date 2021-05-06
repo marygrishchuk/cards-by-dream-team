069bf9debf3c98808897780f9cc9ccd74dd42386
antonymss <demeshkevichanton@gmail.com>
2021-05-06 11:24:55 +0300

    create registration

--- src/features/Register/Register.tsx
+++ src/features/Register/Register.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Register.module.css";
-import {NavLink} from "react-router-dom";
+import {NavLink, Redirect, Switch} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {requestRegister} from "./register-reducer";
+import {AppRootStateType} from "../../app/store";
 
 export const Register = () => {
+    const text = useSelector<AppRootStateType, string>(state => state.register.responseText)
+    const isLogin = useSelector<AppRootStateType, boolean>(state => state.register.isLogin)
+
+    const dispatch = useDispatch()
+    const [emailValue, setEmailValue] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const regData = {email: emailValue, password: password}
+    const setRegister = () => {
+        dispatch(requestRegister(regData))
+    }
+
+    const failedPassword = password !== confirmPassword ? true : false
+
+    if (isLogin) {
+        return <Redirect to={'/login'}/>
+    }
+
     return (
         <div className={style.register}>
+            <h1>{text}</h1>
             Register
-            <input type="email"/>
-            <input type="password"/>
-            <input type="password"/>
-            <button>Register</button>
+            <input type="email" placeholder={'email'} onChange={(e) => setEmailValue(e.currentTarget.value)}/>
+            <input type="password" placeholder={'password'} onChange={(e) => setPassword(e.currentTarget.value)}/>
+            <input type="password" placeholder={'confirm password'}
+                   onChange={(e) => setConfirmPassword(e.currentTarget.value)}/>
+            <button onClick={setRegister} disabled={failedPassword}>Register</button>
             <NavLink to="/login" activeClassName={style.active}>Log in</NavLink>
         </div>
     );
--- src/features/Register/register-reducer.ts
+++ src/features/Register/register-reducer.ts
@@ -1,10 +1,22 @@
+import {authAPI, RegDataType} from "../../api/api";
+import {Dispatch} from "redux";
+import {Simulate} from "react-dom/test-utils";
+
+
 const initialState = {
-    //
+    responseText: '',
+    isLogin: false
 }
 
-export const registerReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
+export const registerReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
         //cases
+        case "REGISTER/SUCCESS_REGISTER": {
+            return {...state, responseText: action.text}
+        }
+        case "REGISTER/SET-LOGIN": {
+            return {...state, isLogin: true}
+        }
         default:
             return state
     }
@@ -12,15 +24,30 @@ export const registerReducer = (state: InitialStateType = initialState, action: 
 
 //action creators
 // export const setSomethingAC = () => ({type: 'REGISTER/SET-SOMETHING'} as const)
-
+const successRegisterAC = (text: string) => ({type: 'REGISTER/SUCCESS_REGISTER', text} as const)
+const setLoginAC = () => ({type: 'REGISTER/SET-LOGIN'} as const)
 //thunk
 // export const doSomethingTC = () => (dispatch: ThunkDispatch) => {
 //
 // }
+export const requestRegister = (regData: RegDataType) => (dispatch: Dispatch) => {
+    authAPI.register(regData)
+        .then((res) => {
+            dispatch(successRegisterAC('success'))
+            setTimeout(() => {
+                dispatch(setLoginAC())
+            }, 3000)
 
+
+        })
+        .catch((err) => {
+            dispatch(successRegisterAC(err.response.data.error))
+        })
+}
 //types
 export type InitialStateType = typeof initialState
 //объединение типов actionов:
 // export type ActionsType = ReturnType<typeof setSomethingAC>
+type ActionsType = ReturnType<typeof successRegisterAC> | ReturnType<typeof setLoginAC>
 // тип диспатча:
 // type ThunkDispatch = Dispatch<ReturnType<typeof setSomethingAC>>
