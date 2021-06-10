2f2271846841a4b2d7916eb71ba0f887c0d00d24
Mary Grishchuk <mary.grishchuk1@gmail.com>
2021-06-10 04:18:12 +0300

    added APIKEY

--- src/features/MapPage/MapPage.tsx
+++ src/features/MapPage/MapPage.tsx
@@ -1,17 +1,19 @@
 import React, {useState} from "react";
 import {Map, Placemark, YMaps} from 'react-yandex-maps';
 import style from "./MapPage.module.css";
+import {config} from "../../config";
 
 
 export const MapPage = () => {
     console.log('render TestMapPage')
     const [coordinates, setCoordinates] = useState([55.684758, 37.738521])
-    const [position, setPosition] = useState<Array<number>>([])
+    // const [position, setPosition] = useState<Array<number>>([])
 
 
     return (
         <div>
             <YMaps query={{
+                apikey: config.MY_API_KEY,
                 ns: 'use-load-option',
                 load:
                     'map.Converter, geoObject.addon.hint',
@@ -21,9 +23,7 @@ export const MapPage = () => {
                     <Map height={370} width={370}
                          state={{center: [55.75, 37.57], zoom: 9}}
                          onClick={(e: any) => setCoordinates([...e.get('coords')])}
-                         properties={{
-                             converter: (e: any) => setCoordinates(e.pageToGlobal(position)),
-                         }}
+                         // modules={['map.Converter']}
                     >
                         <Placemark geometry={[coordinates[0], coordinates[1]]}
                                    options={{draggable: 'true'}}
@@ -31,8 +31,8 @@ export const MapPage = () => {
                                        hintContent: `${coordinates}`,
                                    }}
 
-                                   onDragEnd={(e: any) => {
-                                       setPosition(e.get('position'))
+                                   onDragEnd={() => {
+                                       // setPosition(e.get('position'))
                                    }}
                         />
                     </Map>
