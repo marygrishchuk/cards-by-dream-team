0d16f4dbf8cfd2db275392a78c0b703ab5d286ba
Mary Grishchuk <mary.grishchuk1@gmail.com>
2021-06-10 03:41:03 +0300

    added placemark hint

--- src/features/MapPage/MapPage.tsx
+++ src/features/MapPage/MapPage.tsx
@@ -1,31 +1,43 @@
-import React from "react";
-import {YMaps, Map, Placemark} from 'react-yandex-maps';
+import React, {useState} from "react";
+import {Map, Placemark, YMaps} from 'react-yandex-maps';
+import style from "./MapPage.module.css";
+
 
 export const MapPage = () => {
-    console.log('render TestMapPage');
+    console.log('render TestMapPage')
+    const [coordinates, setCoordinates] = useState([55.684758, 37.738521])
+    const [position, setPosition] = useState<Array<number>>([])
+
+
     return (
         <div>
-            TestMapPage
+            <YMaps query={{
+                ns: 'use-load-option',
+                load:
+                    'map.Converter, geoObject.addon.hint',
+            }}>
+                <div className={style.mapPage}>
 
-            <YMaps>
-                <div>
-                    map div
-
-                    <Map
-                        defaultState={{center: [55.75, 37.57], zoom: 9}}
-
-                        // долго искал эту хрень)
-                        // https://tech.yandex.com/maps/jsapi/doc/2.1/ref/reference/Map-docpage/#field_detail__events
-                        onClick={(e: any) => console.log(e.get('coords'))}
+                    <Map height={370} width={370}
+                         state={{center: [55.75, 37.57], zoom: 9}}
+                         onClick={(e: any) => setCoordinates([...e.get('coords')])}
+                         properties={{
+                             converter: (e: any) => setCoordinates(e.pageToGlobal(position)),
+                         }}
                     >
+                        <Placemark geometry={[coordinates[0], coordinates[1]]}
+                                   options={{draggable: 'true'}}
+                                   properties={{
+                                       hintContent: `${coordinates}`,
+                                   }}
 
-                        {/*https://react-yandex-maps.now.sh/geo-objects/placemark*/}
-                        <Placemark geometry={[55.684758, 37.738521]} />
+                                   onDragEnd={(e: any) => {
+                                       setPosition(e.get('position'))
+                                   }}
+                        />
                     </Map>
                 </div>
 
-                {/*не обязательно в дивке*/}
-                <Map defaultState={{center: [55.75, 37.57], zoom: 9}}/>
             </YMaps>
         </div>
     )
