5dafcb01329b1fba8cca3b4107c5966289d95305
Mary Grishchuk <mary.grishchuk1@gmail.com>
2021-06-11 20:44:01 +0300

    improved test route calculator

--- src/features/MapPage/MapPage.tsx
+++ src/features/MapPage/MapPage.tsx
@@ -10,7 +10,10 @@ export const MapPage = () => {
     const [coordinates, setCoordinates] = useState([55.684758, 37.738521])
     const [destination1, setDestination1] = useState('Moscow, Russia')
     const [destination2, setDestination2] = useState('Minsk, Belarus')
-
+    const [destinations, setDestinations] = useState(['Moscow, Russia', 'Minsk, Belarus'])
+    const onDistanceCheckClick = () => {
+        setDestinations([destination1, destination2])
+    }
 
     return (
         <div>
@@ -27,7 +30,7 @@ export const MapPage = () => {
                     >
                         <Placemark geometry={[coordinates[0], coordinates[1]]} modules={['geoObject.addon.hint']}
                                    properties={{
-                                       hintContent: `${coordinates}`,
+                                       hintContent: `${coordinates[0]}, ${coordinates[1]}`,
                                    }}
                         />
                     </Map>
@@ -35,12 +38,11 @@ export const MapPage = () => {
                         {/*пока пробный подсчет расстояния между двумя точками, используя Yandex API через HOC withYMaps:*/}
                         Route calculation:
                         <div>
-                            <input placeholder="Enter a destination" value={destination1}
-                                   onChange={e => setDestination1(e.currentTarget.value)}/>{' '}
-                            <input placeholder="Enter a destination" value={destination2}
-                                   onChange={e => setDestination2(e.currentTarget.value)}/>
+                            <input value={destination1} onChange={e => setDestination1(e.currentTarget.value)}/>{' '}
+                            <input value={destination2} onChange={e => setDestination2(e.currentTarget.value)}/>
+                            <button onClick={onDistanceCheckClick}>Check distance</button>
                         </div>
-                        <RouteCalculator route={[destination1, destination2]}/>
+                        <RouteCalculator route={destinations}/>
                     </div>
                 </div>
             </YMaps>
